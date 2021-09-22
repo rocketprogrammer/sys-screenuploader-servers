9b393980f4d3cd87ad65dbb6f17025d33850b588
Rocket Programmer <rocketgamer580@gmail.com>
2021-09-21 18:57:25 -0700

    discord_chat: Genius

--- discord_chat/discord_chat.py
+++ discord_chat/discord_chat.py
@@ -27,7 +27,7 @@ def index():
                 break
             f.write(chunk)
 
-    embed = DiscordEmbed(title='Switch', description = "Screenshots from Rocket's Switch", color='03b2f8')
+    embed = DiscordEmbed(url=WEBHOOK_URL, title='Switch', description = "Screenshots from Rocket's Switch", color='03b2f8')
     embed.set_author(name='Nintendo Switch', url='https://nintendo.com', icon_url=ICON_URL)
 
     with open(fpath, "rb") as f:
